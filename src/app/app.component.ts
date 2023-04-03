a09a2171f3f588a355f89f656237952e6fc7b426
Daniela Bru <danielabrudaw@gmail.com>
2023-04-03 16:22:35 +0200

    Primer commit con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'bases';
+  title = 'Contador App';
+  numero = 10;
+
+  base: number = 5;
+
+  acumular(valor: number) {
+    this.numero += valor;
+  }
 }
---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { HeroeComponent } from './heroe/heroe.component';
+import { ListadoComponent } from './listado/listado.component';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [HeroeComponent, ListadoComponent],
+  exports: [ListadoComponent],
+  //Es muy importante poner el "common module" para trabajar con funciones de Angular.
+  imports: [CommonModule],
+})
+export class HeroesModule {}
